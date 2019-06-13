266adfef5c1c6b39006155baa518050d4ecd7d04
jonathaWagner <51007813+jonathaWagner@users.noreply.github.com>
2019-06-13 11:17:36 -0300

    Add files via upload

---
+++ script/trocar.js
@@ -0,0 +1,16 @@
+function reutilizavel (){
+    document.getElementById("trocarGarrafa").src = "../img/garrafaR.jpeg";
+    document.getElementById("trocarPapel").src = "../img/rolosPapelR.jpg";
+    document.getElementById("trocarCanudos").src = "../img/canudosR.jpg";
+    document.getElementById("trocarCaixa").src = "../img/caixaLeiteR.jpg";
+    document.getElementById("trocarPote").src = "../img/potesVidroR.jpg";
+    document.getElementById("trocarTampa").src = "../img/tampaR.jpg";
+}
+function voltar (){
+    document.getElementById("trocarGarrafa").src = "../img/garrafa.jpg";
+    document.getElementById("trocarPapel").src = "../img/roloPapel.png";
+    document.getElementById("trocarCanudos").src = "../img/canudos.jpg";
+    document.getElementById("trocarCaixa").src = "../img/caixaLeite.jpg";
+    document.getElementById("trocarPote").src = "../img/poteVidro.jpg";
+    document.getElementById("trocarTampa").src = "../img/tampa.jpg";
+}
